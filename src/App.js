413cfa7e08682a584a7103690c8fd30f4eebf31f
Lucas Coutinho <lucas.coutinho@eagronom.com>
2022-03-03 08:41:29 -0300

    Add a navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Navbar, Container, NavDropdown, Nav, Button} from 'react-bootstrap'
+import {IoPersonOutline} from 'react-icons/io5'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+          <Navbar.Brand href="#home">Wallet</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto"></Nav>
+          <Nav>
+            <span className="px-2"><Button>Connect</Button></span>
+              <button className="px-2"><span style={{color: "white"}}><IoPersonOutline/></span></button>
+          </Nav>
+      </Navbar.Collapse>
+    </Container>
+    </Navbar>
+</div>
   );
 }
 
